1f8f21a6e6793ebe4ae0e79476ab1d53ccc119d3
jorge-25s <jaserra5@asu.edu>
2024-12-05 00:41:01 -0700

    Update MAE384GroupProjectPart1.m
    Part 3 Finished

--- MAE384GroupProjectPart1.m
+++ MAE384GroupProjectPart1.m
@@ -81,7 +81,6 @@ t_10 = t_10(:);
 ln_I30 = log(I_t(2:end));
 ln_I30 = ln_I30(:);
 ln_I10 = ln_I(1:10);
-
 %30 days
 x_30 = [ones(length(t_30),1), t_30]
 coeffs30 = x_30\ln_I30;
@@ -96,7 +95,14 @@ ln_I0_10 = coeffs_10(1);
 k_10 = coeffs_10(2);    
 I0_est_10 = exp(ln_I0_10); 
 beta_est_10 = (k_10 + gamma_seasonal) * N / S0; 
-
+%Displaying Results
+fprintf('30 days of data:\n');
+fprintf('Estimated I(0): %.2f\n', I0_est_30);
+fprintf('Estimated beta: %.4f\n', beta_est_30);
+fprintf('\n 10 days of data:\n');
+fprintf('Estimated I(0): %.2f\n', I0_est_10);
+fprintf('Estimated beta: %.4f\n', beta_est_10);
+%%%The estimates of I(t) and B improved compared to the true parameters since assumptions made the SIR model match with real-world epidemics. Since the data was altered into a logarithmic scale, the exponential growth was linearized which allows for the intercept (I(t)) and slope(B) to be accurate. 
 
 
 % Function to perform Runge-Kutta 4th Order Method
